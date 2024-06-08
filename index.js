39e79c2d68e23a367788d57a0a0aa8e55e8b710c
jyotsnabauddh01 <jyotsna01bauddh2003@gmail.com>
2024-06-08 11:49:44 +0530

    basic js

---
+++ index.js
@@ -0,0 +1,106 @@
+// //var
+// var num = 10;
+
+// {
+//     num = 20;
+// }
+// console.log("var",num)
+
+// // letvar
+// let num1 = 10
+// {
+//     num1 = 20
+// }
+// console.log("let",num1)
+
+// //const
+// const num2={"name":"raj"}
+
+// {
+//     num2.name="helo"
+// }
+// consol.log("const",num2)
+
+
+//  num = 10
+// {
+//     var num = 20
+//     num = 30
+//     console.log(num)
+
+//     console.log(num)
+
+//     let num = 10
+//     {
+//         let num1=20
+//         num = 30
+//         console.log(num1)
+
+//     }
+//     console.log(num1)
+//     {
+//         const num1=20
+//         console.log(num1)
+//     }
+//     console.log(num2)
+//     {
+//         const num2=20
+//         console.log()
+//     }
+
+//     )
+
+
+// }
+// let num1 = 10;
+// let num2 = 20;
+
+// let mult=num1*num2;
+// console.log(mult)
+
+// let div=num1/num2;
+// console.log(div)
+
+// let add=num1+num2;
+// console.log(add)
+
+// let sub=num1-num2;
+// console.log(sub)
+// let num1 = 5;
+
+// let num2 = 10;
+// let sum = --num1 + ++num2 + num1-- + num2++
+// console.log(sum)
+// console.log(num1)
+// console.log(num2)
+
+// let checkIn = 10
+// let checkOut = -10;
+// let obj ={}
+// let arr;
+// console.log(typeof arr)
+// console.log(typeof obj)
+// console.log(typeof checkIn)
+// console.log(typeof checkOut)
+
+// console.log(checkIn == checkOut);
+
+// let checkIn = "hello"
+// let checkOut = "10";
+// console.log(!checkIn);
+
+
+let checkIn = true
+let checkOut = false;
+console.log(checkOut && checkOut);
+
+let a = 10
+let b = 20;
+let c = 30
+let result = (a < b && a < c) ? "a" : (b < c) ? "b" : "c";
+console.log(result);
+
+let age = 17
+let vote=(age < 18)? "eligible for vote" : "not eligible";
+console.log(vote);
+
