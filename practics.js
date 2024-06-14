51db7977c919de4a732edb5822a4a9aa231358a9
jyotsnabauddh01 <jyotsna01bauddh2003@gmail.com>
2024-06-14 08:43:21 +0530

    first commit

---
+++ practics.js
@@ -0,0 +1,116 @@
+// let age = 10;
+// console.log(age);
+
+// let name = "hello"
+// console.log(name1);
+
+// let name1 = 'it's hello';
+// console.log(name1);
+
+// let name1 ='it"s hello';
+// console.log(name1);
+
+// let isCheck = true
+// console.log(isCheck);
+
+// let person = {"key" :10,"str":"hello"}
+// console.log(person["key"]);
+// console.log(person["str"]);
+
+// let arr =["hello",10]
+// console.log(arr[0]);
+// console.log(arr[1]);
+
+// function add(num){
+    // console.log("my pocket money is "+(num+num+" in current time");
+    // console.log(' my pocket money is $(num+num) in current time');
+// }
+// add(100)
+
+// function add(num){
+    // return num;
+// }
+// const result = add(100);
+// console.log(result);
+
+
+// let age = 17
+
+// if(age > 18){
+//     console.log("greater then 18");
+
+// }
+// else if (age == 18) {
+//     console.log("equal to 18");
+// }
+// else{
+//     console.log("less then 18");
+// }
+
+// let str = "etvar";
+
+// switch(str) {
+    // case "monday":
+        // console.log("today is monday");
+        // break;
+        // case "tuesday":
+            // console.log("today is tuesday");
+            // break;
+            // case "wednesday":
+                // console.log("today is wednesday");
+                // break;
+                // default :
+                // console.log("this is hindi weak day");
+// }
+
+// loop
+// let i = 0;
+// while(i<5){
+    // console.log("hello");
+    // i++;
+
+// }
+
+// let i=5;
+// while(i>0){
+    // console.log("hello");
+    // i--;
+// }
+// let i =0;
+// do{
+    // console.log(i);
+    // i++;
+// 
+// }while (i<5);
+
+// let i=1;
+// do{
+    // console.log(2*i);
+    // i++;
+// }while (i<=10);
+
+// let i;
+// for(i=0; i<5; i++){
+    // console.log("hello");
+// }
+
+// let i;
+// let table =2
+// for(i=1; i<=10; i++){
+    // console.log(`${table}x${i}=${table * i}`);
+// }
+
+// let date = new Date();
+// console.log(date.getFullYear());
+// console.log(date.getDate());
+// console.log(date.getDay());
+// console.log(date.getMonth());
+
+// let i;
+// let table = 3
+// for(i=1; i<=10; i++){
+//     console.log(`${table}x${i}=${table *i}`);
+// }
+
+
+
